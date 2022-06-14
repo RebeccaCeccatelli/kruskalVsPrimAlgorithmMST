d7c7990d8f08ba4aa308c1c1ceb3ea8b2c5af30d
rceccatelli <rebecca.ceccatelli@stud.unifi.it>
2022-06-14 18:29:55 +0200

    Corretto un big in testKruskal e testPrim in Tests e sostituito il valore sentinella 10mila con l'infinito offerto da numpy, np.inf

--- MinHeapNode.py
+++ MinHeapNode.py
@@ -1,9 +1,11 @@
 from UnionFindNode import UnionFindNode
 
+import numpy as np
+
 #inherits from UnionFindfNode because its attributes are needed to compute graph's connectivity (see in method Graph::isConnected())
 class MinHeapNode(UnionFindNode):
 
-    def __init__(self, value, key=10000):
+    def __init__(self, value, key=np.inf):
         super().__init__(value)
         self.key = key      #added attributes
         self.pigreco = None
--- Tests.py
+++ Tests.py
@@ -13,7 +13,7 @@ class MyTestCase(unittest.TestCase):
         matrix1 = [[0,10,12,0,0,0,0,0,0],[10,0,9,8,0,0,0,0,0],[12,9,0,0,3,1,0,0,0],[0,8,0,0,7,0,8,5,0],[0,0,3,7,0,3,0,0,0],[0,0,1,0,3,0,0,6,0],[0,0,0,8,0,0,0,9,2],[0,0,0,5,0,6,9,0,11],[0,0,0,0,0,0,2,11,0]]
         graph1 = Graph(9)
         graph1.setMatrix(matrix1)
-        A = graph1.computeMSTWithKruskal()
+        A,totalWeight = graph1.computeMSTWithKruskal()
 
         expectedA = [2,5,1,  6,8,2,  2,4,3,  3,7,5,  5,7,6,  1,3,8,  3,6,8,  0,1,10]
         i = 0
@@ -28,7 +28,7 @@ class MyTestCase(unittest.TestCase):
         matrix2 = [[0,24,13,13,22],[24,0,22,13,13],[13,22,0,19,14],[13,13,19,0,19],[22,13,14,19,0]]
         graph2 = Graph(5)
         graph2.setMatrix(matrix2)
-        A = graph2.computeMSTWithKruskal()
+        A,totalWeight = graph2.computeMSTWithKruskal()
 
         expectedA = [0,2,13,  0,3,13,  1,3,13,  1,4,13]
         i = 0
@@ -43,7 +43,7 @@ class MyTestCase(unittest.TestCase):
         matrix3 = [[0,9,75,0,0],[9,0,95,19,42],[75,95,0,51,0],[0,19,51,0,31],[0,42,0,31,0]]
         graph3 = Graph(5)
         graph3.setMatrix(matrix3)
-        A = graph3.computeMSTWithKruskal()
+        A,totalWeight = graph3.computeMSTWithKruskal()
 
         expectedA = [0,1,9,  1,3,19,  3,4,31,  2,3,51]
         i = 0
@@ -59,7 +59,7 @@ class MyTestCase(unittest.TestCase):
         matrix1 = [[0,10,12,0,0,0,0,0,0],[10,0,9,8,0,0,0,0,0],[12,9,0,0,3,1,0,0,0],[0,8,0,0,7,0,8,5,0],[0,0,3,7,0,3,0,0,0],[0,0,1,0,3,0,0,6,0],[0,0,0,8,0,0,0,9,2],[0,0,0,5,0,6,9,0,11],[0,0,0,0,0,0,2,11,0]]
         graph1 = Graph(9)
         graph1.setMatrix(matrix1)
-        A = graph1.computeMSTWithPrim(1)
+        A,totalWeight = graph1.computeMSTWithPrim(1)
 
         #different order compared to Kruskal's result but same edges
         expectedA = [0,1,10,  1,3,8,  3,7,5,  7,5,6,  5,2,1,  5,4,3,  3,6,8,  6,8,2]
@@ -75,31 +75,31 @@ class MyTestCase(unittest.TestCase):
         matrix2 = [[0,24,13,13,22],[24,0,22,13,13],[13,22,0,19,14],[13,13,19,0,19],[22,13,14,19,0]]
         graph2 = Graph(5)
         graph2.setMatrix(matrix2)
-        A = graph2.computeMSTWithPrim(2)
+        A,totalWeight = graph2.computeMSTWithPrim(2)
 
         expectedA = [1,3,13,  1,4,13,  3,0,13,  0,2,13]
         i = 0
-        for node in A:
-            self.assertEqual(node.u.value,expectedA[i])
+        for edge in A:
+            self.assertEqual(edge.u.value,expectedA[i])
             i +=1
-            self.assertEqual(node.v.value, expectedA[i])
+            self.assertEqual(edge.v.value, expectedA[i])
             i +=1
-            self.assertEqual(node.weight,expectedA[i])
+            self.assertEqual(edge.weight,expectedA[i])
             i +=1
 
         matrix3 = [[0,9,75,0,0],[9,0,95,19,42],[75,95,0,51,0],[0,19,51,0,31],[0,42,0,31,0]]
         graph3 = Graph(5)
         graph3.setMatrix(matrix3)
-        A = graph3.computeMSTWithPrim(3)
+        A,totalWeight = graph3.computeMSTWithPrim(3)
 
         expectedA = [2,3,51,  3,1,19,  1,0,9,  3,4,31]
         i = 0
-        for node in A:
-            self.assertEqual(node.u.value,expectedA[i])
+        for edge in A:
+            self.assertEqual(edge.u.value,expectedA[i])
             i +=1
-            self.assertEqual(node.v.value,expectedA[i])
+            self.assertEqual(edge.v.value,expectedA[i])
             i +=1
-            self.assertEqual(node.weight, expectedA[i])
+            self.assertEqual(edge.weight, expectedA[i])
             i +=1
 
     def testSameMSTWeight(self):
@@ -113,14 +113,14 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(weightWithKruskal,weightWithPrim)
 
     def testComplexityIncreasingV(self):        #probability is fixed and V goes from 1 to an arbitrary value
-        fixedProbability = 13/100
-        maxV = 350
+        fixedProbability = 10/100
+        maxV = 20
         V = np.arange(1,maxV,1)
 
         kruskalComplexity = [0]*len(V)
         primComplexity = [0]*len(V)
 
-        totExperiments = 3    #repeats the experiment with the same V many times and then computes the mean, for each V
+        totExperiments = 10    #repeats the experiment with the same V many times and then computes the mean, for each V
         for experiment in range (0,totExperiments,1):       #collects data for plotting
             for v in V:
                 index = np.where(V == v)[0][0]
@@ -146,7 +146,7 @@ class MyTestCase(unittest.TestCase):
         primMovingAverage = self.computeMovingAverage(primComplexity)
 
         plt.plot(V,kruskalMovingAverage,'r',V,primMovingAverage,'g')        #plotting commands
-        plt.title("Comparison between Kruskal's and Prim's time complexity, with fixed probability and increasing V\nBig graphs (V from 1 to 100), p = 10")
+        plt.title("Comparison between Kruskal's and Prim's time complexity, with fixed probability and increasing V")
         plt.xlabel("V (number of nodes)")
         plt.ylabel("seconds")
         redPatch = mpatches.Patch(color = 'red', label = "Kruskal's Algorithm for MST")
